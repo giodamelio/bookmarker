b12bd0df9a472a497dff90c431e1165378d09550
Gio d'Amelio <giodamelio@gmail.com>
2023-01-28 21:39:19 +0000

    Add :os_mon to the applications, so LiveviewDashboard can show OS stats

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bookmarker.MixProject do
   def application do
     [
       mod: {Bookmarker.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
